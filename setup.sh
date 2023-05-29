92aa5a3cf50b788ef29bb697407e66dd63b1d305
Theodore Willows <theodoregx@gmail.com>
2023-05-29 15:30:40 -0400

    now using sudo

--- setup.sh
+++ setup.sh
@@ -12,12 +12,6 @@ remove_connection() {
     fi
 }
 
-if [ $(id -u) -ne 0 ]; then
-    echo "Root privledges are required."
-    echo "You must run this script as root."
-    exit 1
-fi
-
 INTERFACE="eth1"
 SSH_PORT=23
 # specify in cidr notation
@@ -43,8 +37,8 @@ done
 echo "Configuring ssh for Hanwha unit."
 echo "Using Static IPv4 address: $STATIC_IP"
 
-nmcli con add con-name "static-$INTERFACE" ifname "$INTERFACE" type ethernet ip4 "$STATIC_IP" gw4 "$GATEWAY"
-nmcli con mod "static-$INTERFACE" ipv4.dns "$GATEWAY,$DNS_SERVER"
+sudo nmcli con add con-name "static-$INTERFACE" ifname "$INTERFACE" type ethernet ip4 "$STATIC_IP" gw4 "$GATEWAY"
+sudo nmcli con mod "static-$INTERFACE" ipv4.dns "$GATEWAY,$DNS_SERVER"
 
 echo "New static connection configured."
 
@@ -52,18 +46,19 @@ if [ -n "$DO_SSH_CONF" ]; then
     echo "Setting up ssh..."
 
     SSHD=/etc/ssh/sshd_config
-    sed -i 's/Port 22/Port '"$SSH_PORT"'/' $SSHD
-    sed -i '/Port '"$SSH_PORT"'/s/^#//g' $SSHD # remove '#', if Port conf is disabled
+    sudo sed -i 's/Port 22/Port '"$SSH_PORT"'/' $SSHD
+    sudo sed -i '/Port'"$SSH_PORT"'/s/^#//g' $SSHD # remove '#', if Port conf is disabled
 
     echo "ssh now on port $SSH_PORT"
 fi
 
 echo "Restarting ssh, Removing previous connection and bringing static connection up..."
-echo "\033[31;1mWARNING\033[0m You may immediately lose connection to host!"
+echo -e "\033[31;1mWARNING\033[0m You may immediately lose connection to host!"
 echo "ping $STATIC_IP to verify interface is UP and that process has completed succesfully."
 echo "Goodbye"
 
-nmcli con up "static-$INTERFACE" iface "$INTERFACE"
-systemctl restart ssh
+sudo remove_connection "$INTERFACE"
+sudo nmcli con up "static-$INTERFACE" iface "$INTERFACE"
+sudo systemctl restart ssh
 
 exit 0
